a67bbdc62875d19b5c00d2360ba214f5c1f4264a
Pablo Deeleman <pablo.deeleman@dynatrace.com>
2021-01-07 22:54:57 +0100

    feat: add  jsdoc headers to alert types

--- src/models/alert-notification.ts
+++ src/models/alert-notification.ts
@@ -20,8 +20,21 @@ export enum AlertNotificationType {
  * An CPU load alert notification depicting a given alert type, when was it started, when was emitted and how many times it has occured.
  */
 export interface AlertNotification {
+  /**
+   * Alert notification type, signaling whether this object is heavy load alert or a recovery alert.
+   */
   type: AlertNotificationType;
+  /**
+   * UTC timestamp from the moment this CPU load scenario started.
+   */
   createdOn: number;
+  /**
+   * UTC timestamp from the moment this alert notification was emitted.
+   */
   emittedOn?: number;
+  /**
+   * Collection of CPU load records that originated this alert scenario.
+   * This signals how many times the CPU has notified this scenario in the scope of this alert.
+   */
   cpuLoadRecords: CpuLoadRecord[];
 }
