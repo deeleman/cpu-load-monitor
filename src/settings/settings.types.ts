ef70cbdfd08c3de3762a049a3cc7230961a45c09
Pablo Deeleman <pablo.deeleman@dynatrace.com>
2021-01-07 22:44:12 +0100

    feat: add jsdoc headers to setting tokens

--- src/settings/settings.types.ts
+++ src/settings/settings.types.ts
@@ -1,14 +1,47 @@
+/**
+ * @description
+ * Settings configuration for API consumption and CPU load data refresh
+ */
 export interface PollingSettings {
+  /**
+   * API request polling rate in milliseconds.
+   */
   refreshRate: number;
+  /**
+   * Maximum time window allocated for historical CPU load information in milliseconds.
+   */
   expirationWindow: number;
+  /**
+   * Maximum amount of items to be handled by objects persisting CPU load data items.
+   */
   bufferSize: number;
+  /**
+   * PAth or URL to REST API endpoint serving CPU load data
+   */
   pollingEndpoint: string;
 }
 
+/**
+ * @description
+ * Settings configuration for alert notifications buffer time and warning threshold
+ */
 export interface AlertSettings {
+  /**
+   * Minimum time required to consider that a CPU has recovered from heavy load, in milliseconds.
+   */
   cpuRecoveryNotificationThreshold: number;
+  /**
+   * Minimum time required to consider that a CPU is under heavy load, in milliseconds.
+   */
   cpuOverloadAlertingThreshold: number;
+  /**
+   * Average CPU load average rate that tells apart normal from heavy CPU load
+   */
   cpuLoadAverageThreshold: number;
 }
 
+/**
+ * @description
+ * Global settings configuration type encompassing polling and alerting settings.
+ */
 export type Settings = PollingSettings & AlertSettings;
