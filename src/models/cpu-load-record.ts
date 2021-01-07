584823ec1d0f59cff6d984b9d29efd07f5959d94
Pablo Deeleman <pablo.deeleman@dynatrace.com>
2021-01-07 22:48:24 +0100

    feat: add JSDOC headers to main interface

--- src/models/cpu-load-record.ts
+++ src/models/cpu-load-record.ts
@@ -4,7 +4,16 @@
  * both in UTC and readable human format.
  */
 export interface CpuLoadRecord {
+  /**
+   * CPU load average during the last minute
+   */
   loadAvg: number;
+  /**
+   * UTC timestamp of the record creation
+   */
   timestamp: number;
+  /**
+   * UTC timestamp in human readable format (HH:mm:ss)
+   */
   timeLabel: string;
 }
